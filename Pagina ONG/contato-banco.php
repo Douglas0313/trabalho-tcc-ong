9e1423a9b0d4011094ff5647a3d07d297db56165
douglas de souza santos <82132463+Douglas0313@users.noreply.github.com>
2023-05-17 22:12:52 -0300

    atualização página contatos e adicionamento do icone no logins

---
+++ Pagina ONG/contato-banco.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'config-banco-apoia.php';
+$mensagem = filter_input(INPUT_POST, 'mensagem');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = filter_input(INPUT_POST, 'nome');
+    $telefone = filter_input(INPUT_POST, 'telefone');
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $preferencia_contato = filter_input(INPUT_POST, 'Contato');
+    $horario = filter_input(INPUT_POST, 'horario');
+
+    $sql = $pdo->prepare("INSERT INTO contatos (nome, email, telefone, mensagem, preferencia_contato, horario) VALUES (:nome, :email, :telefone, :mensagem, :preferencia_contato, :horario)");
+    $sql->bindValue(':nome', $nome);
+    $sql->bindValue(':telefone', $telefone);
+    $sql->bindValue(':email', $email);
+    $sql->bindValue(':mensagem', $mensagem);
+    $sql->bindValue(':preferencia_contato', $preferencia_contato);
+    $sql->bindValue(':horario', $horario);
+    
+    if ($sql->execute()) {
+        echo '<script>alert("Mensagem enviada com sucesso!. Entraremos em contato o mais breve possível, obrigado")</script>';
+    } else {
+        echo '<script>alert("Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente mais tarde.")</script>';
+    }
+}
+?>
+
